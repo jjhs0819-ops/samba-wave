ff60567ba693c5edc7274ab9adba9333572914e5
showpang <sbk0674@gmail.com>
2026-04-16 11:28:58 +0900

    fix: merge migration 파일 ruff 포맷 적용

--- backend/alembic/versions/37e835a156d6_merge_cafe24_and_multitenancy_heads.py
+++ backend/alembic/versions/37e835a156d6_merge_cafe24_and_multitenancy_heads.py
@@ -9,7 +9,6 @@ Create Date: 2026-04-16 11:26:37.294830
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
 revision: str = "37e835a156d6"
 down_revision: Union[str, Sequence[str], None] = ("d9c4ff7d6d2e", "c4f2407a01")
